255be6604f3b327ad8a99c4e12eec4a204af1e4a
Muhammad Morsy <muhammadmorsy5@gmail.com>
2024-08-13 14:42:36 -0700

    edit pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,9 @@ that module Create a program that uses the Reddit API
 to retrieve the number of subscribers.
 """
 
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
@@ -15,15 +17,16 @@ def number_of_subscribers(subreddit):
     """
 
     api_url = "https://www.reddit.com/r/"
-    
+
     url = '{}{}/about.json'.format(api_url, subreddit)
-    
-    headers={"User-Agent": "request"}
-    
+
+    headers = {"User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"}
+
     res = requests.get(
-    url,
-    headers=headers,
-    allow_redirects=False
+        url,
+        headers=headers,
+        allow_redirects=False
     )
     if res.status_code == 200:
         return results.json()['data']['subscribers']
